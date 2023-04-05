47d284b000a44a689d158be3e6446cbeef7ab104
dannylewie <129995310+dannylewie@users.noreply.github.com>
2023-04-05 15:46:31 -0400

    First website upload

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Charter",
+    artist: "Prod. Danny Lewie",
+    img: "Charter",
+    src: "Charter"
+  },
+  {
+    name: "Run It Up",
+    artist: "Prod. Danny Lewie",
+    img: "RunItUp",
+    src: "RunItUp"
+  },
+  {
+    name: "Hello World",
+    artist: "Prod. Danny Lewie",
+    img: "HelloWorld",
+    src: "HelloWorld"
+  },
+  {
+    name: "Cranes",
+    artist: "Prod. Danny Lewie",
+    img: "Cranes",
+    src: "Cranes"
+  },
+  {
+    name: "Either way",
+    artist: "Prod. Danny Lewie",
+    img: "EitherWay",
+    src: "EitherWay"
+  },
+  {
+    name: "Food",
+    artist: "Prod. Danny Lewie",
+    img: "Food",
+    src: "Food"
+  },
+  // {
+  //   name: "Hello world",
+  //   artist: "Prod. Danny Lewie",
+  //   img: "music-4",
+  //   src: "Hello World"
+  // },
+  {
+    name: "I Need Love",
+    artist: "Prod. Danny Lewie",
+    img: "INeedLove",
+    src: "INeedLove"
+  },  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
