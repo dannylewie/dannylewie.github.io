a9ec62fbe2a5779bd31c45ac3290befc365adeda
dannylewie <129995310+dannylewie@users.noreply.github.com>
2023-04-11 12:00:37 -0400

    Beats Update 4-11

--- js/music-list.js
+++ js/music-list.js
@@ -46,18 +46,97 @@ let allMusic = [
     img: "Food",
     src: "Food"
   },
-  // {
-  //   name: "Hello world",
-  //   artist: "Prod. Danny Lewie",
-  //   img: "music-4",
-  //   src: "Hello World"
-  // },
+  {
+    name: "Fortunate",
+    artist: "Prod. Danny Lewie",
+    img: "Fortunate",
+    src: "Fortunate"
+  },
   {
     name: "I Need Love",
     artist: "Prod. Danny Lewie",
     img: "INeedLove",
     src: "INeedLove"
   },  
+  {
+    name: "Getaway",
+    artist: "Prod. Danny Lewie",
+    img: "Getaway",
+    src: "Getaway"
+  },
+  {
+    name: "Ima Player",
+    artist: "Prod. Danny Lewie",
+    img: "ImaPlayer",
+    src: "ImaPlayer"
+  },
+  {
+    name: "Its Dark",
+    artist: "Prod. Danny Lewie",
+    img: "ItsDark",
+    src: "ItsDark"
+  },
+  {
+    name: "Jay Kizzy",
+    artist: "Prod. Danny Lewie",
+    img: "JayKizzy",
+    src: "JayKizzy"
+  },
+  {
+    name: "Lonnie",
+    artist: "Prod. Danny Lewie",
+    img: "Lonnie",
+    src: "Lonnie"
+  },
+  {
+    name: "Meter",
+    artist: "Prod. Danny Lewie",
+    img: "Meter",
+    src: "Meter"
+  },
+  {
+    name: "Mild",
+    artist: "Prod. Danny Lewie",
+    img: "Mild",
+    src: "Mild"
+  },
+  {
+    name: "NewLife",
+    artist: "Prod. Danny Lewie",
+    img: "NewLife",
+    src: "NewLife"
+  },
+  {
+    name: "NoWonda",
+    artist: "Prod. Danny Lewie",
+    img: "NoWonda",
+    src: "NoWonda"
+  },
+  {
+    name: "Savages",
+    artist: "Prod. Danny Lewie",
+    img: "Savages",
+    src: "Savages"
+  },
+  {
+    name: "Somber",
+    artist: "Prod. Danny Lewie",
+    img: "Somber",
+    src: "Somber"
+  },
+  {
+    name: "Summer",
+    artist: "Prod. Danny Lewie",
+    img: "Summer",
+    src: "Summer"
+  },
+  {
+    name: "TheBop",
+    artist: "Prod. Danny Lewie",
+    img: "TheBop",
+    src: "TheBop"
+  },
+
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
